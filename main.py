e2da5d4b6f6f32843ede9353e5377bcf2069d790
Nikolai Perlt <nikolaiperlt@outlook.com>
2022-09-30 13:57:38 +0000

    mvp

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from graphviz import Digraph
-
 from diagrams import Diagram
-from diagrams.k8s.network import Ingress, Service
 from diagrams import Node
 
 from test_project.core import core
@@ -12,16 +9,18 @@ import astroid
 
 
 def test_module_check():
-    api_node = BTNode(connected_code=api, label="api")
-    core_node = BTNode(connected_code=core, label="core")
-    controller_node = BTNode(connected_code=controller, label="controller")
+    with Diagram("Test project diagram", show=False):
+        api_node = BTNode(connected_code=api, label="api")
+        core_node = BTNode(connected_code=core, label="core")
+        controller_node = BTNode(connected_code=controller, label="controller")
+        third_party_api = BTNode(label="Awesome API")
 
-    nodes = [api_node, core_node, controller_node]
+        nodes = [api_node, core_node, controller_node, third_party_api]
 
-    api_node >> core_node
-    api_node >> controller_node
-
-    # core_node >> controller_node
+        api_node >> core_node
+        api_node >> controller_node
+        core_node >> controller_node
+        api_node >> third_party_api
 
     for node in nodes:
         if not node.validate():
@@ -37,21 +36,30 @@ class BTNode:
 
     _edge_to: list["BTNode"] = []
     _ast = None
+    diagram_node: Node = None
 
-    def __init__(self, connected_code, label: str):
+    def __init__(self, label: str, connected_code=None):
         self.connected_code = connected_code
         self.label = label
-        self._ast: astroid.Module = astroid.MANAGER.ast_from_module(
-            connected_code
-        )
+        self._ast = None
+        if self.connected_code is not None:
+            self._ast: astroid.Module = astroid.MANAGER.ast_from_module(
+                connected_code
+            )
         self._edge_to = []
+        self.diagram_node = Node(label=label)
 
     def validate(self) -> bool:
+        if self._ast is None:
+            return True
+
         for sub_node in self._ast.body:
             if isinstance(sub_node, astroid.node_classes.ImportFrom):
                 sub_node: astroid.node_classes.ImportFrom = sub_node
                 if sub_node.modname not in [
-                    edge._ast.name for edge in self._edge_to
+                    edge._ast.name
+                    for edge in self._edge_to
+                    if edge._ast is not None
                 ]:
                     return False
         return True
@@ -59,27 +67,8 @@ class BTNode:
     def __rshift__(self, other: "BTNode"):
         self._edge_to.append(other)
         print(f"{self.label} -> {other.label}")
+        self.diagram_node >> other.diagram_node
 
-
-# def main():
-#     print("start")
-#     with Diagram("Grouped Workers", show=True):
-#         api_node = BTNode(label="api", connected_code=api)
-#         core_node = BTNode(label="core", connected_code=core)
-#         test_node = BTNode(label="test")
-#         graph = api_node >> core_node >> test_node
-#
-#         dot: Digraph = api_node._diagram.dot
-#         dot_body = dot.body
-#         edges = [edge for edge in dot_body if "->" in edge]
-#         res = {}
-#         for edge in edges:
-#             l_edge = edge.split("->")[0]
-#             r_edge = edge.split("->")[1]
-#
-#         dot_edges = dot.edge_attr
-#         print("hej")
-#
 
 if __name__ == "__main__":
     # main()
