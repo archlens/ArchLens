0682b2e19cd0fcda3a3bd10d7d6a600831dd9183
Perlt <33089800+Perlten@users.noreply.github.com>
2023-02-14 20:51:51 +0100

    Feature/pip (#1)

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BT-diagrams",
+    version="0.0.5",
+    description="Thesis project",
+    author="Nikolai Perlt",
+    author_email="npe@itu.dk",
+    url="https://github.com/Perlten/Master-thesis-rename",
+    packages=find_packages(),
+    long_description="This is the long description",
+    install_requires=["plantuml", "typer", "astroid", "six"],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+    entry_points={
+        "console_scripts": [
+            "bt-diagrams=src.cli_interface:main",
+        ],
+    },
+)
