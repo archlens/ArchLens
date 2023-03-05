4c42e2e17c396f628e7660168e847298c204612d
Perlt <33089800+Perlten@users.noreply.github.com>
2023-03-05 20:34:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MT-diagrams",
-    version="0.1.1",
+    version="0.1.2",
     description="Thesis project",
     author="Nikolai Perlt",
     author_email="npe@itu.dk",
