4ea1b8e60d6dfa0624e8ca971feb13fbe70789f4
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-06-23 13:17:45 +0300

    added CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.2.5] - 2025-06-23
+### Fixed
+- Added changelog
+
+## [0.2.4] - 2025-06-23
+### Fixed
+- Fixed package depth functionality that was not working properly (with Sebastian Cloos Hylander)
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import os
 # Read README for long description
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
         readme = f.read()
-    with open(os.path.join(here, 'CHANGELOG.md'), encoding='utf-8') as f:
+    with open(os.path.join(here, "CHANGELOG.md"), encoding="utf-8") as f:
         changelog = f.read()
     long_description = f"{readme}\n\n{changelog}"
 except FileNotFoundError:
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 setup(
     name="ArchLens",
-    version="0.2.4",
+    version="0.2.5",
     description="Designed for visualizing package dependencies and highlighting differences between"
     " branches in GitHub pull requests. It offers customization options to tailor package views.",
     author="The ArchLens Team",
