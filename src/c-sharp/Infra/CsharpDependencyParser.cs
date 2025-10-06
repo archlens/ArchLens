5ceedfe7cacbc63505993afd1a8f4ffbd81ef44b
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-10-06 11:48:29 +0200

    chore: moved using collector logic into CsharpDependencyParser Co-authored-by: lottejd@gmail.com

--- src/c-sharp/Infra/CsharpDependencyParser.cs
+++ src/c-sharp/Infra/CsharpDependencyParser.cs
@@ -1,19 +1,51 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Archlens.Domain.Interfaces;
 
 namespace Archlens.Infra;
 
-class CsharpDependencyParser : IDependencyParser
+class CsharpDependencyParser(string _projectName) :  IDependencyParser
 {
-    public Task<IReadOnlyList<string>> ParseFileDependencies(string path, CancellationToken ct = default)
+    public async Task<IReadOnlyList<string>> ParseFileDependencies(string path, CancellationToken ct = default)
     {
-        throw new System.NotImplementedException();
+        /*
+            open file from given path
+            match regex "Using.ProjectName"
+            take all matches and put in list
+            return list
+        */
+        List<string> usings = [];
+
+        try
+        {
+            StreamReader sr = new(path);
+
+            string line = await sr.ReadLineAsync(ct);
+
+            while (line != null)
+            {
+                line = await sr.ReadLineAsync(ct);
+                // match regex + add to usings
+            }
+
+            sr.Close();
+            return usings;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+            return [];
+        }
+
     }
 
     public Task<IReadOnlyList<string>> ParseModuleDependencies(string path, CancellationToken ct = default)
     {
+        //for each file in module --> Parse file dependencies
+        //for each module in module --> Parse module dependencies
         throw new System.NotImplementedException();
     }
 }
