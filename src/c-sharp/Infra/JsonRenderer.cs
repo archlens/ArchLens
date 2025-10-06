1dbd27d1bf78dbf819eebbf34e0dbf0d61fcbea8
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-10-06 11:45:40 +0200

    chore: moved json converter logic into json renderer Co-authored-by: lottejd@gmail.com

--- src/c-sharp/Infra/JsonRenderer.cs
+++ src/c-sharp/Infra/JsonRenderer.cs
@@ -8,8 +8,44 @@ namespace Archlens.Infra;
 
 public sealed class JsonRenderer : IRenderer
 {
-    public Task<string> RenderGraphAsync(DependencyGraph graph, Options options, CancellationToken ct = default)
+    public string RenderGraphAsync(DependencyGraph graph, Options options, CancellationToken ct = default)
     {
-        throw new System.NotImplementedException();
+        var packagestr = "";
+        for (int i = 0; i < graph.packages().Count; i++)
+        {
+            var package = graph.packages()[i];
+
+            if (packagestr.Contains(package)) continue;
+
+            var comma = "";
+            if (i < graph.packages().Count - 2) comma = ",";
+
+            packagestr +=
+                $$"""
+                
+                {
+                    "name": "{{package}}",
+                    "state": "NEUTRAL"
+                }{{comma}}
+
+            """;
+        }
+
+        var str =
+        $$"""
+        {
+            "title": "{{graph.Name}}",
+            "packages": [
+                {{packagestr}}
+            ],
+
+            "edges": [
+            {{graph.ToJson()}}
+
+            ]
+        }
+        """;
+        return str;
     }
+
 }
