50120c01323e80da02afd8b9d2b355ec61fcaf13
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-10-06 11:50:56 +0200

    chore: fitted dependency graph building logic into arch skeleton Co-authored-by: lottejd@gmail.com

--- src/c-sharp/Application/DependencyGraphBuilder.cs
+++ src/c-sharp/Application/DependencyGraphBuilder.cs
@@ -1,122 +1,58 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Archlens.Domain;
 using Archlens.Domain.Interfaces;
 using Archlens.Domain.Models;
 
 namespace Archlens.Application;
-// Builds a dependency graph
 
 public class DependencyGraphBuilder(IDependencyParser _dependencyParser)
 {
-    public async Task<DependencyGraph> BuildGraphAsync(IReadOnlyList<string> files, CancellationToken ct = default)
+    public async Task<DependencyGraph> GetGraphAsync(string root, IReadOnlyList<string> changedModules, CancellationToken ct = default)
     {
-        // Parse files (parallel if you like), construct a graph object
-        throw new NotImplementedException();
-    }
-}
-
-class DependencyGraphBuilderOld(
-    string projectName,
-    string root,
-    List<string> excludes)
-{
-    private string _root = root;
-    private List<string> _excludes = excludes;
-
-    // Start graph build
-    public DependencyGraph GetGraph()
-    {
-        string[] dir = Directory.GetDirectories(_root);
+        string[] dir = Directory.GetDirectories(root);
 
         Node graph = new() { Name = "root", Children = [], Dependencies = [] };
 
-        BuildGraph(dir, graph);
+        await BuildGraphAsync(graph, changedModules, ct);
 
         return graph;
     }
 
-
-    // Recursively build graph
-    public void BuildGraph(string[] directories, Node graph)
+    private async Task BuildGraphAsync(Node root, IReadOnlyList<string> changedModules, CancellationToken ct = default)
     {
         List<Node> children = [];
 
-        foreach (var directory in directories)
+        foreach (var module in changedModules)
         {
-            if (_excludes.Exists(directory.Contains)) continue;
 
-            Node node = new() { Name = directory.Split("\\").Last(), Children = [], Dependencies = [] };
+            Node node = new() { Name = module.Split("\\").Last(), Children = [], Dependencies = [] };
 
-            string[] files = Directory.GetFiles(directory);
+            string[] files = Directory.GetFiles(module);
 
             foreach (string file in files)
             {
-                if (file.EndsWith(".cs"))
+                var usings = await _dependencyParser.ParseFileDependencies(file, ct);
+
+                Leaf child = new() { Dependencies = usings, Name = file.Split("\\").Last() };
+
+                node.AddChild(child);
+
+                foreach (var dep in child.Dependencies)
                 {
-                    Leaf child = CreateLeaf(file);
-                    node.AddChild(child);
-                    foreach (var dep in child.Dependencies)
-                    {
-                        node.AddDependency(dep, child);
-                    }
+                    node.AddDependency(dep, child);
                 }
             }
 
-            BuildGraph(Directory.GetDirectories(directory), node);
+            string[] submodules = Directory.GetDirectories(module);
+
+            await BuildGraphAsync(node, submodules, ct);
 
             children.Add(node);
         }
 
-        graph.AddChildren(children);
-    }
-
-
-    // Create and return leaf with dependencies
-    private Leaf CreateLeaf(string filename)
-    {
-        string lines = "";
-        List<string> usings = [];
-
-        try
-        {
-            StreamReader sr = new StreamReader(filename);
-
-            string line = sr.ReadLine();
-
-            while (line != null)
-            {
-                lines += "\n" + line;
-                line = sr.ReadLine();
-            }
-
-            sr.Close();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
-        }
-
-        SyntaxTree tree = CSharpSyntaxTree.ParseText(lines);
-        CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
-
-        var collector = new UsingCollector(projectName);
-        collector.Visit(root);
-
-        foreach (var directive in collector.Usings)
-        {
-            usings.Add(directive.Name.ToString());
-        }
-
-        Leaf leaf = new() { Dependencies = usings, Name = filename.Split("\\").Last() };
-
-        return leaf;
+        root.AddChildren(children);
     }
 }
