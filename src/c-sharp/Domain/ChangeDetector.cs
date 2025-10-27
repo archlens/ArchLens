fdc0895044a9ba1c26febf8feb8ec95ce94fd510
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-10-27 15:16:31 +0100

    fix: Update `ChangeDetector` to return dictionary of modules and files

--- src/c-sharp/Domain/ChangeDetector.cs
+++ src/c-sharp/Domain/ChangeDetector.cs
@@ -17,45 +17,58 @@ public sealed class ChangeDetector
         string[] FileSuffixes      // specific file postfixes, like: "*.dev.cs", ".g.cs"
     );
 
-    public static async Task<IReadOnlyList<string>> GetChangedProjectFilesAsync(
+    public static async Task<IReadOnlyDictionary<string, IEnumerable<string>>> GetChangedProjectFilesAsync(
         Options options,
         DependencyGraph lastSavedGraph,
         CancellationToken ct = default)
     {
-        var projectRoot = Path.GetFullPath(options.ProjectRoot);
+        var projectRoot = String.IsNullOrEmpty(options.FullRootPath) ? Path.GetFullPath(options.ProjectRoot) : options.FullRootPath;
 
         var candidates = EnumerateFiles(projectRoot, options.FileExtensions);
 
-        var files = candidates.ToArray();
-
+        Dictionary<string, IEnumerable<string>> modules = [];
         if (options.Exclusions != null && options.Exclusions.Count > 0)
         {
-            var exclusions = CompileExclusions(options.Exclusions);
-            files = [.. candidates.Where(path => !IsExcluded(projectRoot, path, exclusions))];
+            var rules = CompileExclusions(options.Exclusions);
+            foreach(var module in candidates.Keys)
+            {
+                if (module == projectRoot)
+                    continue;
+                var isExcludedModule = IsExcluded(projectRoot, module, rules);
+                if (!isExcludedModule)
+                {
+                    var contents = candidates[module].Where(content => !IsExcluded(projectRoot, content, rules));
+                    modules.Add(module, contents);
+                }
+            }
+        }
+        else
+        {
+            modules = candidates;
         }
 
-        var changed = new List<string>();
+        var changed = new Dictionary<string, IEnumerable<string>>();
         var thread = new SemaphoreSlim(Environment.ProcessorCount - 1);
-
-        var tasks = files.Select(async file =>
+        var tasks = modules.Select(async pair =>
         {
             ct.ThrowIfCancellationRequested();
             await thread.WaitAsync(ct).ConfigureAwait(false);
             try
             {
-                var relativePath = GetRelative(projectRoot, file);
+                var relativePath = GetRelative(projectRoot, pair.Key);
                 var inLastGraph = lastSavedGraph.Packages().Contains(relativePath);
                 if (!inLastGraph)
                 {
-                    changed.Add(relativePath);
+                    changed.Add(relativePath, pair.Value);
                 }
                 else
                 {
                     var lastNodeWriteTime = lastSavedGraph.GetChild(relativePath).LastWriteTime;
-                    var currentWriteTime = File.GetLastWriteTimeUtc(file);
+
+                    var currentWriteTime = File.GetLastWriteTimeUtc(pair.Key);
 
                     if (currentWriteTime > lastNodeWriteTime)
-                        changed.Add(relativePath);
+                        changed.Add(relativePath, pair.Value);
                 }
             }
             finally
@@ -68,10 +81,15 @@ public sealed class ChangeDetector
         return changed;
     }
 
-    private static IEnumerable<string> EnumerateFiles(string root, IReadOnlyList<string> extensions)
+    private static Dictionary<string, IEnumerable<string>> EnumerateFiles(string root, IReadOnlyList<string> extensions)
     {
         var dirs = new Stack<string>();
         dirs.Push(root);
+
+        var result = new Dictionary<string, IEnumerable<string>>
+        {
+            { root, [] }
+        };
 
         while (dirs.Count > 0)
         {
@@ -79,23 +97,25 @@ public sealed class ChangeDetector
 
             IEnumerable<string> subdirs = [];
             try { 
+
                 subdirs = Directory.EnumerateDirectories(dir);
             } catch { /* ignore */ }
             
-            foreach (var subdir in subdirs) 
+            foreach (var subdir in subdirs)
                 dirs.Push(subdir);
+            
+            result[dir] = [..subdirs];
 
             IEnumerable<string> files = [];
             try { 
                 files = Directory.EnumerateFiles(dir);
             } catch { /* ignore */ }
 
-            foreach (var file in files)
-            {
-                if (extensions.Contains(Path.GetExtension(file)))
-                    yield return file;
-            }
+            var includedFiles = files.Where(file => extensions.Contains(Path.GetExtension(file))).Select(file => GetRelative(root, file).Split('/').Last()).ToList();
+            result[dir] = [.. includedFiles];
         }
+        result.Remove(root);
+        return result;
     }
 
 
@@ -151,17 +171,15 @@ public sealed class ChangeDetector
         );
     }
 
-    private static bool IsExcluded(string projectRoot, string absolutePath, ExclusionRule rules)
+    private static bool IsExcluded(string projectRoot, string content, ExclusionRule rules)
     {
-        var relativePath = GetRelative(projectRoot, absolutePath);
+        var path = GetRelative(projectRoot, content);
 
-        var fileName = Path.GetFileName(relativePath);
+        if (rules.DirPrefixes.Any(rule => rule.StartsWith(path, StringComparison.OrdinalIgnoreCase)))
+            return true;
 
-        foreach (var prefix in rules.DirPrefixes)
-            if (relativePath.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-                return true;
 
-        var segments = relativePath.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        var segments = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
         foreach (var segment in segments)
         {
             foreach (var ban in rules.Segments)
@@ -171,6 +189,7 @@ public sealed class ChangeDetector
             }
         }
 
+        var fileName = Path.GetFileName(path);
         foreach (var suf in rules.FileSuffixes)
             if (fileName.EndsWith(suf, StringComparison.OrdinalIgnoreCase))
                 return true;
