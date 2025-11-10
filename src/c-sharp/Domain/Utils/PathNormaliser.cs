4decf43be96c20c00f9efbf397a02c051bfd9902
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-11-10 10:18:21 +0100

    feat: add PathNormaliser

---
+++ src/c-sharp/Domain/Utils/PathNormaliser.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Archlens.Domain.Utils;
+
+public static class PathNormaliser
+{
+    public static string NormalizePath(string root, string path)
+    {
+        var isModule = IsDirectoryPath(path);
+        var relativePath = Path.GetRelativePath(root, path).Replace(Path.DirectorySeparatorChar, '/');
+        if (isModule)
+            return  $"./{relativePath}/";
+        return $"./{relativePath}";
+    }
+
+    public static bool IsDirectoryPath(string path)
+    {
+        bool isDir = false;
+        try
+        {
+            isDir = (File.GetAttributes(path) & FileAttributes.Directory) != 0;
+        }
+        catch { /* ignore */ }
+        return isDir;
+    }
+}
