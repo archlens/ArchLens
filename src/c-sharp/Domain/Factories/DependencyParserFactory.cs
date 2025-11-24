e24b701f56bad76b01e99ddeef51de7619266aa5
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-11-24 12:56:47 +0100

    fix: use syntaxwalker
    Co-authored-by: Lotte lottejd@gmail.com

--- src/c-sharp/Domain/Factories/DependencyParserFactory.cs
+++ src/c-sharp/Domain/Factories/DependencyParserFactory.cs
@@ -1,16 +1,15 @@
-namespace Archlens.Domain.Factories;
-
 using System;
 using Archlens.Domain.Interfaces;
 using Archlens.Domain.Models.Enums;
 using Archlens.Domain.Models.Records;
 using Archlens.Infra;
 
+namespace Archlens.Domain.Factories;
 public sealed class DependencyParserFactory
 {
     public static IDependencyParser SelectDependencyParser(Options o) => o.Language switch
     {
-        Language.CSharp => new CsharpDependencyParser(o),
+        Language.CSharp => new CsharpSyntaxWalkerParser(o),
         _ => throw new ArgumentOutOfRangeException(nameof(o.Language))
     };
 }
